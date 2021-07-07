56416a0cc0ae12af814c3c6028e229986b7f886f
RalonPlayz <79267114+RalonPlayz@users.noreply.github.com>
2021-07-07 15:30:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+status = "";
+video = "";
+
+
+function preload()
+{
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(470, 370);
+    canvas.center()
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocosd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    video.loop()
+    video.speed(1);
+    video.volume(0);
+}
+
+function draw()
+{
+    image(video, 0, 0, 470, 370);
+}
